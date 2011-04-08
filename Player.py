c99d1c6ac579e56483997b49bb417d9517e673f2
jbeorse <jeffisveryhungry@gmail.com>
2011-04-08 14:55:45 -0700

    Added skeleton classes

---
+++ Player.py
@@ -0,0 +1,21 @@
+class Player:
+	RED=1
+	BLACK=2
+	
+	#Fields
+	#-------
+	#String name	The name of the player
+	#String color	The color of the player ("Black, White")
+	#int colorNum	The color number of the player
+	
+	#Constructor
+	def __init__(self):
+	
+	
+	#Gets a start Point and destination Point from the user boolean indicates if 
+	#the last move attempt was a failure						
+	def getMove(boolean):
+	
+	#Runs a single turn of this player.  If return is true, player has another
+	#turn.  This method should be composed of calls on getMove checkMove and Move
+	def singleTurn():
