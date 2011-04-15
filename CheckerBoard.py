2c12af480193a6033a304a8cb57656be98a33123
Jian M <jianmov@gmail.com>
2011-04-14 21:26:01 -0700

    Add checkmove() and move() method to CheckerBoard

--- CheckerBoard.py
+++ CheckerBoard.py
@@ -63,15 +63,33 @@ class CheckerBoard:
 	def gameWinner(whoseTurnLast):
 		return Player.RED
 	
-	# Checks to see if the given move is legal (i.e. it is diagonal one square, there is no other
-	# piece in the end point, or it is a jump, etc). 
+	# Checks to see if the given move is legal.  
 	# Inputs are two Point objects: the start point and the end point
-	def checkMove(start, end):
-		return False
+	# Pre: 1)start and end are points in the 8*8 board 2)GameState.get_state() is 1 or 2 (either player is playing)
+	def checkMove(self, start, end):
+		if start.x%2 + start.y%2 != 1 or end.x%2 + end.y%2 !=1:
+			return False
+
+		if not self.board[start.x][start.y] == GameState.get_state()*2-1 or not self.board[end.x][end.y] = SquareState.EMPTY:
+			return False
+
+		if end.y - start.y == abs(end.y - start.y)*(GameState.get_state()*2-3) and not SquareState(start) == 2 and not SquareState(start) == 4:
+			return False
+
+		if abs(end.y - start.y) == 1 and abs(end.x - start.x) == 1 and not anyJump(GameState.get_state()):
+			return True
+
+		if abs(end.y - start.y) == 2 and abs(end.x - start.x) == 2 and self.board[(start.x + end.x)/2][(start.y + end.y)/2] == 3 - GameState.get_state():
+			return True
+    return False    
 	
 	# Makes the given move.  Returns true if the player has another move, else false
 	# Inputs are two point objects: the start point and the end point
 	def move(start, end):
+		self.board[end.x][end.y] = self.board[start.x][start.y]
+		self.board[start.x][start.y] = SquareState.EMPTY
+		if anyJump():
+			return True
 		return False
 	
 	# Returns a list of all available Points that can be moved to from Start
@@ -82,5 +100,48 @@ class CheckerBoard:
 	# Returns a list of all possible moves this player can make
 	def getAllMoves(whoseTurn):
 		return None
+	
+	# private function 
+	# Return ture if there are any pieces that can jump
+	def anyJump(self):
+		for row in range(len(self.board)):
+            for col in range(len(self.board[row])):
+			start = new Point(row, col)
+			step = 0
+			if canJump(start, step):
+				return true
+		return False
+    
+	# private function 	
+	# Return true if user can any jump from a particular start
+	def canJumps(self, start):
+		if step == 4:
+			return False
+		if start.x%2 + start.y%2 != 1 or x%2 + y%2 !=1:
+			return False
+		if not self.board[start.x][start.y] == GameState.get_state()*2-1:
+			return False
+		if step == 3:
+			x = start.x + 2
+			y = start.y + 2
+		elif mode = 2:
+			x = start.x + 2
+			y = start.y - 2
+		elif mode = 1:
+			x = start.x - 2
+			y = start.y + 2
+		else:
+			x = start.x - 2
+			y = start.y - 2
+		if x <= 7 and x >= 0 and y <= 7 and y >= 0:
+			if end.y - start.y != abs(end.y - start.y)*(GameState.get_state()*2-3) or SquareState(start) == 2 or SquareState(start) == 4:
+				if abs(end.y - start.y) == 2 and abs(end.x - start.x) == 2 and self.board[(start.x + end.x)/2][(start.y + end.y)/2] == 3 - GameState.get_state():
+					if self.board[end.x][end.y] != SquareState.EMPTY:
+						return True
+		step += 1
+			
+			
+	
+
 	
 	
